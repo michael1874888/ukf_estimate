efed1ce41ac77b203a98aa64e42d687bc989cb86
ncrl <michael1874888@gmail.com>
2018-11-26 20:59:37 +0800

    measurement_flag

--- scripts/target_trajectory_qp_3deg.py
+++ scripts/target_trajectory_qp_3deg.py
@@ -9,9 +9,10 @@ from collections import deque
 import matplotlib.pyplot as plt
 import time
 from geometry_msgs.msg import Point
+from std_msgs.msg import Float32MultiArray
 
 poly_degree = 3
-window_length = 60
+window_length = 100
 regulator_weight = 1
 
 time = deque(maxlen = window_length)
@@ -29,6 +30,10 @@ current_time = rospy.get_time()
 previous_time = rospy.get_time()
 dt = 0.
 
+current_time2 = rospy.get_time()
+previous_time2 = rospy.get_time()
+dt2 = 0.
+
 
 def cvxopt_solve_qp(P, q, G=None, h=None, A=None, b=None):
     P = .5 * (P + P.T)  # make sure P is symmetric
@@ -44,24 +49,34 @@ def cvxopt_solve_qp(P, q, G=None, h=None, A=None, b=None):
 
 
 def callback(msg):
-    global callback_flag, target_data, current_time, previous_time, ti
-    callback_flag = True
+    global target_data, current_time, previous_time, ti#, callback_flag
+    #callback_flag = True
     target_data = msg
     current_time = rospy.get_time()
     dt = current_time - previous_time
     #print(dt)
     previous_time = current_time
     ti = ti + dt
-    #rospy.Rate(30).sleep()
+
+
+def callback2(box):
+    global callback_flag
+    callback_flag = True
+
+    if box.data[0] <= 0.8:
+        callback_flag = False
+
+
     
     
 
 
 def quadratic_progamming():
 
-    global ti, time, P_, qx, qy, qz
+    global ti, time, P_, qx, qy, qz, coeff_x, coeff_y, coeff_z, current_time2, previous_time2
     
-    rospy.Subscriber("/estimate_data", output, callback)   
+    estimate_sub = rospy.Subscriber("/estimate_data", output, callback)
+    box_sub = rospy.Subscriber("/YOLO/box", Float32MultiArray, callback2)
     pub_pos = rospy.Publisher('target_qp_pos', Point, queue_size=1)
     pub_vel = rospy.Publisher('target_qp_vel', Point, queue_size=1)
     pub_acc = rospy.Publisher('target_qp_acc', Point, queue_size=1)
@@ -72,98 +87,142 @@ def quadratic_progamming():
     
     while not rospy.is_shutdown():
         #time1 = rospy.get_time()
+        
+        current_time2 = rospy.get_time()
+        dt2 = current_time2 - previous_time2
+        previous_time2 = current_time2
+    
         if callback_flag is True:
+            print('qp calculating')
             time.append(ti)
             target_position.append(target_data.target_pose)
             target_velocity.append(target_data.target_vel)
             #print(time)
             #print(len(time))
             #print(target_position)
+            
+            if len(time) == window_length:
 
-        if len(time) == window_length:
-
-            ti = ti - time[0]
-            
-            t0 = time[0]
-            for i in range(window_length):
-                time[i] = time[i] - t0
-            
-            
-            P_ = np.zeros(((poly_degree+1),(poly_degree+1)))
-            qx = np.zeros(((poly_degree+1), 1))
-            qy = np.zeros(((poly_degree+1), 1))
-            qz = np.zeros(((poly_degree+1), 1))
-            
-            for i in range(window_length):
-            
-    	        t = time[i]
-                Ti0 = np.mat([[1],[t],[t**2],[t**3]])
-                Tie0 = Ti0*Ti0.T
-                Ti1 = np.mat([[0],[1],[2*t],[3*t**2]])
-                Tie1 = Ti1*Ti1.T
-                P_ = P_ + Tie0 + Tie1
-                qx = qx + -2*target_position[i].x*Ti0 + -2*target_velocity[i].x*Ti1
-                qy = qy + -2*target_position[i].y*Ti0 + -2*target_velocity[i].y*Ti1
-                qz = qz + -2*target_position[i].z*Ti0 + -2*target_velocity[i].z*Ti1
-
-
-            t_last = time[-1]
-            t_init = time[0]
-            
-            #Tir for acceleration regulator
-            Tir_last = np.mat([[0, 0,           0,            0],
-                               [0, 0,           0,            0],
-                               [0, 0,    4*t_last,  6*t_last**2],
-                               [0, 0, 6*t_last**2, 12*t_last**3]])
-                               
-                               
-            Tir_init = np.mat([[0, 0,           0,            0],
-                               [0, 0,           0,            0],
-                               [0, 0,    4*t_init,  6*t_init**2],
-                               [0, 0, 6*t_init**2, 12*t_init**3]])
-                  
-            Tir = Tir_last - Tir_init
-    
-		    #Tir = np.array(Tir).astype(np.float64)
-            
-            P_ = P_ + window_length*regulator_weight*Tir
-		    #extract 1/2 to be the standard form, so we need to multiply 2 to the original formula
-            P = 2*P_
-            #print(P)
-            #print(q)
+                ti = ti - time[0]
+                
+                t0 = time[0]
+                for i in range(window_length):
+                    time[i] = time[i] - t0
+                
+                
+                P_ = np.zeros(((poly_degree+1),(poly_degree+1)))
+                qx = np.zeros(((poly_degree+1), 1))
+                qy = np.zeros(((poly_degree+1), 1))
+                qz = np.zeros(((poly_degree+1), 1))
+                
+                for i in range(window_length):
+                
+                    t = time[i]
+                    Ti0 = np.mat([[1],[t],[t**2],[t**3]])
+                    Tie0 = Ti0*Ti0.T
+                    Ti1 = np.mat([[0],[1],[2*t],[3*t**2]])
+                    Tie1 = Ti1*Ti1.T
+                    P_ = P_ + Tie0 + Tie1
+                    qx = qx + -2*target_position[i].x*Ti0 + -2*target_velocity[i].x*Ti1
+                    qy = qy + -2*target_position[i].y*Ti0 + -2*target_velocity[i].y*Ti1
+                    qz = qz + -2*target_position[i].z*Ti0 + -2*target_velocity[i].z*Ti1
 
 
-            coeff_x = cvxopt_solve_qp(P, qx)
-            coeff_y = cvxopt_solve_qp(P, qy)
-            coeff_z = cvxopt_solve_qp(P, qz)
+                t_last = time[-1]
+                t_init = time[0]
+                
+                #Tir for acceleration regulator
+                Tir_last = np.mat([[0, 0,           0,            0],
+                                   [0, 0,           0,            0],
+                                   [0, 0,    4*t_last,  6*t_last**2],
+                                   [0, 0, 6*t_last**2, 12*t_last**3]])
+                                   
+                                   
+                Tir_init = np.mat([[0, 0,           0,            0],
+                                   [0, 0,           0,            0],
+                                   [0, 0,    4*t_init,  6*t_init**2],
+                                   [0, 0, 6*t_init**2, 12*t_init**3]])
+                      
+                Tir = Tir_last - Tir_init
+        
+		        #Tir = np.array(Tir).astype(np.float64)
+                
+                P_ = P_ + window_length*regulator_weight*Tir
+		        #extract 1/2 to be the standard form, so we need to multiply 2 to the original formula
+                P = 2*P_
+                #print(P)
+                #print(q)
 
-            target_pos_poly_x = coeff_x[0] + coeff_x[1]*t_last + coeff_x[2]*t_last**2 + coeff_x[3]*t_last**3
-            target_pos_poly_y = coeff_y[0] + coeff_y[1]*t_last + coeff_y[2]*t_last**2 + coeff_y[3]*t_last**3
-            target_pos_poly_z = coeff_z[0] + coeff_z[1]*t_last + coeff_z[2]*t_last**2 + coeff_z[3]*t_last**3
-            target_vel_poly_x = coeff_x[1] + 2*coeff_x[2]*t_last + 3*coeff_x[3]*t_last**2
-            target_vel_poly_y = coeff_y[1] + 2*coeff_y[2]*t_last + 3*coeff_y[3]*t_last**2
-            target_vel_poly_z = coeff_z[1] + 2*coeff_z[2]*t_last + 3*coeff_z[3]*t_last**2
-            target_acc_poly_x = 2*coeff_x[2] + 3*2*coeff_x[3]*t_last
-            target_acc_poly_y = 2*coeff_y[2] + 3*2*coeff_y[3]*t_last
-            target_acc_poly_z = 2*coeff_z[2] + 3*2*coeff_z[3]*t_last
+
+                coeff_x = cvxopt_solve_qp(P, qx)
+                coeff_y = cvxopt_solve_qp(P, qy)
+                coeff_z = cvxopt_solve_qp(P, qz)
+
+                target_pos_poly_x = coeff_x[0] + coeff_x[1]*t_last + coeff_x[2]*t_last**2 + coeff_x[3]*t_last**3
+                target_pos_poly_y = coeff_y[0] + coeff_y[1]*t_last + coeff_y[2]*t_last**2 + coeff_y[3]*t_last**3
+                target_pos_poly_z = coeff_z[0] + coeff_z[1]*t_last + coeff_z[2]*t_last**2 + coeff_z[3]*t_last**3
+                target_vel_poly_x = coeff_x[1] + 2*coeff_x[2]*t_last + 3*coeff_x[3]*t_last**2
+                target_vel_poly_y = coeff_y[1] + 2*coeff_y[2]*t_last + 3*coeff_y[3]*t_last**2
+                target_vel_poly_z = coeff_z[1] + 2*coeff_z[2]*t_last + 3*coeff_z[3]*t_last**2
+                target_acc_poly_x = 2*coeff_x[2] + 3*2*coeff_x[3]*t_last
+                target_acc_poly_y = 2*coeff_y[2] + 3*2*coeff_y[3]*t_last
+                target_acc_poly_z = 2*coeff_z[2] + 3*2*coeff_z[3]*t_last
+                
+                target_qp_pos.x = target_pos_poly_x
+                target_qp_pos.y = target_pos_poly_y
+                target_qp_pos.z = target_pos_poly_z
+                target_qp_vel.x = target_vel_poly_x
+                target_qp_vel.y = target_vel_poly_y
+                target_qp_vel.z = target_vel_poly_z
+                target_qp_acc.x = target_acc_poly_x
+                target_qp_acc.y = target_acc_poly_y
+                target_qp_acc.z = target_acc_poly_z
+                #print(coeff)
+                #print(time)
+
+                pub_pos.publish(target_qp_pos)
+                pub_vel.publish(target_qp_vel)
+                pub_acc.publish(target_qp_acc)
+                #time2 = rospy.get_time()
+                #print((time2-time1))
+                
+                
+        else:
+            print('qp estimating')
+            if len(time) == window_length:
+                    
+                t_last = t_last + dt2
+                    
+                target_pos_poly_x = coeff_x[0] + coeff_x[1]*t_last + coeff_x[2]*t_last**2 + coeff_x[3]*t_last**3
+                target_pos_poly_y = coeff_y[0] + coeff_y[1]*t_last + coeff_y[2]*t_last**2 + coeff_y[3]*t_last**3
+                target_pos_poly_z = coeff_z[0] + coeff_z[1]*t_last + coeff_z[2]*t_last**2 + coeff_z[3]*t_last**3
+                target_vel_poly_x = coeff_x[1] + 2*coeff_x[2]*t_last + 3*coeff_x[3]*t_last**2
+                target_vel_poly_y = coeff_y[1] + 2*coeff_y[2]*t_last + 3*coeff_y[3]*t_last**2
+                target_vel_poly_z = coeff_z[1] + 2*coeff_z[2]*t_last + 3*coeff_z[3]*t_last**2
+                target_acc_poly_x = 2*coeff_x[2] + 3*2*coeff_x[3]*t_last
+                target_acc_poly_y = 2*coeff_y[2] + 3*2*coeff_y[3]*t_last
+                target_acc_poly_z = 2*coeff_z[2] + 3*2*coeff_z[3]*t_last
             
-            target_qp_pos.x = target_pos_poly_x
-            target_qp_pos.y = target_pos_poly_y
-            target_qp_pos.z = target_pos_poly_z
-            target_qp_vel.x = target_vel_poly_x
-            target_qp_vel.y = target_vel_poly_y
-            target_qp_vel.z = target_vel_poly_z
-            target_qp_acc.x = target_acc_poly_x
-            target_qp_acc.y = target_acc_poly_y
-            target_qp_acc.z = target_acc_poly_z
-            #print(coeff)
-            #print(time)
+                target_qp_pos.x = target_pos_poly_x
+                target_qp_pos.y = target_pos_poly_y
+                target_qp_pos.z = target_pos_poly_z
+                target_qp_vel.x = target_vel_poly_x
+                target_qp_vel.y = target_vel_poly_y
+                target_qp_vel.z = target_vel_poly_z
+                target_qp_acc.x = target_acc_poly_x
+                target_qp_acc.y = target_acc_poly_y
+                target_qp_acc.z = target_acc_poly_z
+                #print(coeff)
+                #print(time)
 
-            pub_pos.publish(target_qp_pos)
-            pub_vel.publish(target_qp_vel)
-            pub_acc.publish(target_qp_acc)
-            #time2 = rospy.get_time()
-            #print((time2-time1))
+                pub_pos.publish(target_qp_pos)
+                pub_vel.publish(target_qp_vel)
+                pub_acc.publish(target_qp_acc)
+                #time2 = rospy.get_time()
+                #print((time2-time1))
+            
+            #callback_flag = False
+            
         rate.sleep()
 
 
